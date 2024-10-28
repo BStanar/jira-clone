3c5655decd9ce802e151804ccd6aeb0b2747283c
Boki <borisstanar1@gmail.com>
2024-10-28 23:29:50 +0100

    Can delete workspaces

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -106,14 +106,14 @@ const app = new Hono()
             const {name, image} = c.req.valid("form");
         
             const member = await getMember({
-                databases,
-                workspaceId,
-                userId: user.$id
-            });
+				databases,
+				workspaceId,
+				userId: user.$id,
+			});
 
-            if(!member || member.role !== MemberRole.ADMIN){
-                return c.json({error: "Unauthorized"}, 401);
-            }
+            if (!member || member.role !== MemberRole.ADMIN) {
+				return c.json({ error: "Unauthorized" }, 401);
+			}
 
             let uploadedImageUrl: string | undefined;
 
@@ -146,6 +146,34 @@ const app = new Hono()
             return c.json({data: workspace});
         }
         
+    ).delete(
+        "/:workspaceId",
+        sessionMiddleWare,
+        async (c) => {
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const { workspaceId } = c.req.param();
+
+            const member = await getMember({
+				databases,
+				workspaceId,
+				userId: user.$id,
+			});
+
+            if(!member || member.role !== MemberRole.ADMIN){
+               return c.json({error: "unauthorizedkkkkk"}, 401); 
+            }
+            
+            //TODO: DELETE MEMBERS, PROJECTS AND TASKS
+            await databases.deleteDocument(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId,
+            );
+
+            return c.json({data: {$id: workspaceId}});
+        }
     );
 
 export default app;
