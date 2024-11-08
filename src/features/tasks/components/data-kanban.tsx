db921601e333b6cc02ce8e86dc344c0d7cb12257
Boki <borisstanar1@gmail.com>
2024-11-08 11:40:08 +0100

    Made data kanban

---
+++ src/features/tasks/components/data-kanban.tsx
@@ -0,0 +1,61 @@
+import { DragDropContext } from "@hello-pangea/dnd";
+import { Task , TaskStatus} from "../types";
+import React, { useCallback, useEffect, useState } from "react";
+import { KanbanColumnHeader } from "./kanban-column-header";
+
+interface DataKanbanProps {
+   data: Task[];  
+};
+
+const boards: TaskStatus[] = [
+   TaskStatus.BACKLOG,
+   TaskStatus.TODO,
+   TaskStatus.IN_PROGRESS,
+   TaskStatus.IN_REVIEW,
+   TaskStatus.DONE,
+];
+
+type TaskState = {
+   [key in TaskStatus]: Task[];
+};
+
+export const DataKanban = ({
+   data
+}: DataKanbanProps) => {
+
+   const [tasks, setTasks] = useState<TaskState>(()=>{
+      const initialTasks: TaskState = {
+         [TaskStatus.BACKLOG]: [],
+         [TaskStatus.TODO]: [],
+         [TaskStatus.IN_PROGRESS]: [],
+         [TaskStatus.IN_REVIEW]: [],
+         [TaskStatus.DONE]: [],
+      };
+
+      data.forEach((task) =>{
+         initialTasks[task.status].push(task);
+      });
+
+      Object.keys(initialTasks).forEach((status) => {
+         initialTasks[status as TaskStatus].sort((a,b) => a.position-b.position);
+      });
+
+      return initialTasks;
+   })
+   return(
+      <DragDropContext onDragEnd={() => {}}>
+         <div className="flex overflow-x-auto">
+            {boards.map((board)=> {
+               return(
+                  <div key={board} className="flex-1 mx-2 bg-muted p-1.5 rounded-md min-w-[200px]">
+                     <KanbanColumnHeader
+                        board={board}
+                        taskCount={tasks[board].length}
+                     />
+                  </div>
+               )
+            })}
+         </div>
+      </DragDropContext>
+   );
+}
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -13,6 +13,7 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
 
@@ -93,8 +94,7 @@ export const TaskViewSwitcher = () => {
                      <DataTable columns={columns} data={tasks?.documents ?? []}/>
                   </TabsContent>
                   <TabsContent value="kanban" className="mt-0">
-                     kanban 
-                     {JSON.stringify(tasks)}
+                     <DataKanban data={tasks?.documents ?? []}/>
                   </TabsContent>
                   <TabsContent value="calendar" className="mt-0">
                      {JSON.stringify(tasks)}
