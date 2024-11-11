3591f293db804ac6fb24b85234e04940dd6092b4
Boki <borisstanar1@gmail.com>
2024-11-11 13:57:19 +0100

    Made data calendar

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,7 @@ import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTasks } from "../api/use-update-bulk-tasks";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
 
@@ -112,8 +113,8 @@ export const TaskViewSwitcher = () => {
                   <TabsContent value="kanban" className="mt-0">
                      <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []}/>
                   </TabsContent>
-                  <TabsContent value="calendar" className="mt-0">
-                     {JSON.stringify(tasks)}
+                  <TabsContent value="calendar" className="mt-0 h-full pb-4">
+                     <DataCalendar data={tasks?.documents ?? [] }/>
                   </TabsContent>
                </>
             )}
