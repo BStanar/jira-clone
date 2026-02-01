e811579679a5edae49a2b839368fdd129bba10d1
Boris Stanar <126799702+BStanar@users.noreply.github.com>
2026-02-01 12:12:48 +0100

    Update sign-in-card.tsx

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -50,55 +50,10 @@ export const SignInCard = () => {
         <Card className="w-full h-full md:w-[477px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl">
-                    Welcome back
+                    Pozdrav Polina. Da, hiša ni prosta v tem obdobju. Poster nisam na vrijeme prihvatio ili odbio vas zahtjev za registraciju. Airbnb mi je blokirao te datume na njihovoj platformi. Na google mapama mozete naci moj broj. Ovo je moj broj +39762881786 pa se mozemo cuti preko vibera ili whatsapa. Airbnb ne dozvoljava slanje poruka broja preko poruka.
                 </CardTitle>
             </CardHeader>
-            <div className="px-7 mb-2">
-                <DottedSeparator/>
-            </div>
-            <CardContent className="p-7">
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                    <FormField
-                            name="email"
-                            control={form.control}
-                            render={({ field }) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <Input
-                                                {...field}
-                                                type="email"
-                                                placeholder="Enter your email"
-                                            />
-                                        </FormControl>
-                                        <FormMessage/>
-                                    </FormItem>
-                                    )}
-                        />
-                        <FormField
-                            name="password"
-                            control={form.control}
-                            render={({ field }) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <Input
-                                                {...field}
-                                                type="password"
-                                                placeholder="Enter password"
-                                            />
-                                        </FormControl>
-                                        <FormMessage/>
-                                    </FormItem>
-                                    )}
-                        />
-                    
-                    
-                    <Button disabled={isPending} size="lg" className="w-full">
-                        Login
-                    </Button>
-                    </form>
-                </Form>
-            </CardContent>
+           
             <div className="px-2">
                 <DottedSeparator></DottedSeparator>
             </div>
