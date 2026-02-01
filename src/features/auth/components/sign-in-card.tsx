1e24a0482424de475ab34070f00d8421ad1d8cd2
Boris Stanar <126799702+BStanar@users.noreply.github.com>
2026-02-01 12:14:18 +0100

    Update sign-in-card.tsx

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -56,40 +56,7 @@ export const SignInCard = () => {
            
             <div className="px-2">
                 <DottedSeparator></DottedSeparator>
-            </div>
-            <CardContent className="p-7 flex flex-col gap-y-4">
-            <Button 
-                    onClick={() => signUpWithGoogle()}
-                    disabled={isPending}
-                    variant="secondary"
-                    size="lg"
-                    className="w-full"
-                >
-                    <FcGoogle className="mr-2 size-5"/>
-                    Login with Google
-                </Button>
-                <Button 
-                    onClick={() => signUpWithGithub()}
-                    disabled={isPending}
-                    variant="secondary"
-                    size="lg"
-                    className="w-full"
-                >
-                    <FaGithub className="mr-2 size-5"/>
-                    Login with Github
-                </Button>
-            </CardContent>
-            <div className="px-7">
-                <DottedSeparator/>
-            </div>
-            <CardContent className="p-7 flex items-center justify-center">
-                <p>
-                    Don&apos;t have an account?
-                    <Link href="/sign-up">
-                    <span className="text-blue-700">&nbsp;Sign up</span>
-                    </Link>
-                </p>
-            </CardContent>
+            </div>            
         </Card>
 );
 };
